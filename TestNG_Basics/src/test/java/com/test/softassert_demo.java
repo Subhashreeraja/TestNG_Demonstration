b0e91d4958049f30c14e0563a1762de685121ff8
Subhashree <subhashreerajait@gmail.com>
2026-04-19 23:05:02 +0530

    updated softassert files

--- TestNG_Basics/src/test/java/com/test/softassert_demo.java
+++ TestNG_Basics/src/test/java/com/test/softassert_demo.java
@@ -10,87 +10,89 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class softassert_demo {
-		
-		  public WebDriver driver;
-		  @BeforeMethod
-		  public void beforeTest() {
-			  ChromeOptions options=new ChromeOptions();
-			  options.addArguments("--start-maximized");
-			  options.addArguments("--headless");  
-			  driver = new ChromeDriver(options); 
-			  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-			  driver.get("https://www.demoblaze.com/");
-			 
-		  }
-		    
-		  @Test()
-		  public void validation() {
-			  driver.navigate().refresh(); 
-			  driver.findElement(By.id("login2")).click();
-			  driver.findElement(By.id("loginusername")).sendKeys("Admin");
-			  driver.findElement(By.id("loginpassword")).sendKeys("admin");
-			  driver.findElement(By.xpath("//button[text()='Log in']")).click();
-			  
-			  WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(15));
-			  WebElement welcome=wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("nameofuser")));
-		      String expected="Welcome Admin";
-		      String actual=welcome.getText();
-			  Assert.assertEquals(actual,expected); 	  
-		  }
-		 
 
-		  
-		  @Test
-		  public void logininvalid1() {
-			  driver.navigate().refresh();
-		      driver.findElement(By.id("login2")).click();
-		      driver.findElement(By.id("loginusername")).sendKeys("");
-		      driver.findElement(By.id("loginpassword")).sendKeys("admin");
-		      driver.findElement(By.xpath("//button[text()='Log in']")).click();
+    public WebDriver driver;
+    SoftAssert softAssert = new SoftAssert(); 
 
-		      WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		      Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+    @BeforeMethod
+    public void beforeTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("---start-maximized--");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://demoblaze.com/");
+    }
 
-		      String actual = alert.getText();
-		      String expected = "Please fill out Username and Password.";
+    @Test
+    public void validation() {
+        
 
-		      Assert.assertEquals(actual, expected);
-		      alert.accept();
-		  }
-		  
-		  
-		  @Test
-		  public void logininvalid2() {
-			  driver.navigate().refresh(); 
-		      driver.findElement(By.id("login2")).click();
-		      driver.findElement(By.id("loginusername")).sendKeys("Admin");
-		      driver.findElement(By.id("loginpassword")).sendKeys("1");
-		      driver.findElement(By.xpath("//button[text()='Log in']")).click();
+        driver.findElement(By.id("login2")).click();
+        driver.findElement(By.id("loginusername")).sendKeys("Admin");
+        driver.findElement(By.id("loginpassword")).sendKeys("admin");
+        driver.findElement(By.xpath("//button[@onclick='logIn()']")).click();
 
-		      WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		      Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        WebElement message = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("nameofuser")));
 
-		      String actual = alert.getText();
-		      String expected = "Wrong password."; 
+        String msg = message.getText();
+        String msg1 = "Welcome Admin";
 
-		      Assert.assertEquals(actual, expected);
+        softAssert.assertEquals(msg1, msg, "Login successful"); 
+        softAssert.assertAll();                                  
+    }
 
-		      alert.accept();
-		  }
-			  
-			  
-			 
-			
-		  @AfterMethod
-		  public void afterTest() {
-			  driver.quit();
-		  }
-	
-	}
+    @Test
+    public void invalidusername() {
+       
 
+        driver.findElement(By.id("login2")).click();
+        driver.findElement(By.id("loginusername")).sendKeys("Sho");
+        driver.findElement(By.id("loginpassword")).sendKeys("admin");
+        driver.findElement(By.xpath("//button[@onclick='logIn()']")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = driver.switchTo().alert();
+
+        String msg = alert.getText();
+        String msg1 = "User does not exist.";
+        alert.accept();
+
+        softAssert.assertEquals(msg1, msg, "Login failed");  
+        softAssert.assertAll();                               
+    }
+
+    @Test
+    public void invalidpassword() {
+       
+
+        driver.findElement(By.id("login2")).click();
+        driver.findElement(By.id("loginusername")).sendKeys("Admin");
+        driver.findElement(By.id("loginpassword")).sendKeys("sho");
+        driver.findElement(By.xpath("//button[@onclick='logIn()']")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = driver.switchTo().alert();
+
+        String msg = alert.getText();
+        String msg1 = "Wrong password.";
+        alert.accept();
+
+        softAssert.assertEquals(msg1, msg, "Login failed");
+        softAssert.assertAll();                              
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        driver.quit();
+    }
+}
